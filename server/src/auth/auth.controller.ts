00cd584cd0c206c1018d6db0121137cd259b8401
AndreySVRF <andrey.smagin@rokolabs.com>
2022-07-16 03:47:14 +0400

    SERVER feat(auth): Added logic for signin

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {AuthService} from './auth.service';
-import {AuthDto} from './dto';
+import {AuthSigninPostDto, AuthSignupPostDto} from './dto';
 
 @Controller('auth')
 class AuthController {
 	constructor(private authService: AuthService) {}
 
 	@Post('signup')
-	signup(@Body() dto: AuthDto) {
+	signup(@Body() dto: AuthSignupPostDto) {
 		return this.authService.signup(dto);
 	}
 
 	@Post('signin')
-	signin() {
-		return this.authService.signin();
+	signin(@Body() dto: AuthSigninPostDto) {
+		return this.authService.signin(dto);
 	}
 }
 
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,15 +2,17 @@ import {ForbiddenException, Injectable} from '@nestjs/common';
 import * as argon from 'argon2';
 import {PrismaClientKnownRequestError} from '@prisma/client/runtime';
 import {PrismaService} from '../prisma';
-import {AuthDto} from './dto';
+import {AuthSigninPostDto, AuthSignupPostDto} from './dto';
 
 const CREDENTIALS_TAKEN = 'Credentials taken';
+const USER_EMAIL_NOT_FOUND = 'User with this email not found';
+const INCORRECT_PASSWORD = 'Incorrect password';
 
 @Injectable({})
 class AuthService {
 	constructor(private prisma: PrismaService) {}
 
-	async signup(dto: AuthDto) {
+	async signup(dto: AuthSignupPostDto) {
 		const hash = await argon.hash(dto.password);
 
 		try {
@@ -36,8 +38,26 @@ class AuthService {
 		}
 	}
 
-	signin() {
-		return {msg: 'signin'};
+	async signin(dto: AuthSigninPostDto) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email,
+			},
+		});
+
+		if (!user) {
+			throw new ForbiddenException(USER_EMAIL_NOT_FOUND);
+		}
+
+		const pwMatches = await argon.verify(user.password, dto.password);
+
+		if (!pwMatches) {
+			throw new ForbiddenException(INCORRECT_PASSWORD);
+		}
+
+		delete user.password;
+
+		return user;
 	}
 }
 
