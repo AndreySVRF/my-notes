376ea0fb6cb532d83b550a40e8bb4a76f0a3e191
AndreySVRF <andrey.smagin@rokolabs.com>
2022-07-15 22:45:01 +0400

    SERVER feat(auth): Added registration feat

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,12 +1,39 @@
-import {Injectable} from '@nestjs/common';
+import {ForbiddenException, Injectable} from '@nestjs/common';
+import * as argon from 'argon2';
+import {PrismaClientKnownRequestError} from '@prisma/client/runtime';
 import {PrismaService} from '../prisma';
+import {AuthDto} from './dto';
+
+const CREDENTIALS_TAKEN = 'Credentials taken';
 
 @Injectable({})
 class AuthService {
 	constructor(private prisma: PrismaService) {}
 
-	signup() {
-		return {msg: 'signup'};
+	async signup(dto: AuthDto) {
+		const hash = await argon.hash(dto.password);
+
+		try {
+			const user = await this.prisma.user.create({
+				data: {
+					email: dto.email,
+					password: hash,
+					firstName: dto.firstName,
+					lastName: dto.lastName,
+				},
+			});
+
+			delete user.password;
+
+			return user;
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ForbiddenException(CREDENTIALS_TAKEN);
+				}
+			}
+			throw error;
+		}
 	}
 
 	signin() {
