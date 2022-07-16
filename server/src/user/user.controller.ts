9795555cb1763f3b90a0557e69df40762f2ff624
AndreySVRF <andrey.smagin@rokolabs.com>
2022-07-16 19:32:24 +0400

    SERVER feat(user): Added logic for update user

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,18 +1,24 @@
-import {Controller, Get, Patch, UseGuards} from '@nestjs/common';
+import {Body, Controller, Get, Patch, UseGuards} from '@nestjs/common';
 import {User} from '@prisma/client';
 import {JwtGuard} from '../auth/guard';
 import {GetUser} from '../auth/decorator';
+import {UserService} from './user.service';
+import {UserUpdatePatchDto} from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 class UserController {
+	constructor(private userService: UserService) {}
+
 	@Get('me')
 	getMe(@GetUser() user: User) {
 		return user;
 	}
 
-	@Patch()
-	editUser() {}
+	@Patch('me')
+	update(@GetUser('id') id: number, @Body() dto: UserUpdatePatchDto) {
+		return this.userService.update(id, dto);
+	}
 }
 
 export {UserController};
